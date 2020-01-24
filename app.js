bb8d6b29fbd3df585c56f067bf2b72f587527c68
Julia Aasmaa <my_e_10@hotmail.com>
2020-01-24 10:26:50 +0200

    commit

--- app.js
+++ app.js
@@ -5,7 +5,25 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(event){
-    console.log("button clicked");
+    const userNumber = document.querySelector('input[type="number"]').value;
+    
+    //GET request
+    fetch(`http://api.icndb.com/jokes/random/${userNumber}`)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        let output = '';
+        console.log(data.value);
+        //for each joke in data.value array
+        data.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`;
+            console.log(joke.joke);
+        });
+
+        document.querySelector('.jokes').innerHTML = output;
+    });
 
     event.preventDefault();
 }
